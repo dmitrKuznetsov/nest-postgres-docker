1aa80bebe0df9047964d026d4305425f187183f5
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-04-29 16:10:24 +0300

    Authentication Module: loginning and registration JSON Tokens Password encryption

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { UsersModule } from './../users/users.module';
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.SECRET || 'SECRET',
+      signOptions: {
+        expiresIn: '24h'
+      }
+    })
+  ]
+})
+export class AuthModule {}
