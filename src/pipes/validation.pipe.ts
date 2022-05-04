693f94f80bd348f8d431a9fb380b399e9f1ba6ec
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-05-04 16:34:01 +0300

    Added Global ValidationPipe Added ValidationException

---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,21 @@
+import { ValidationException } from './../exception/validation.exception';
+import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, metadata: ArgumentMetadata) : Promise<any> {
+    const obj = plainToClass(metadata.metatype, value)
+    const errors = await validate(obj)
+
+    if (errors.length) {
+      let messages = errors
+      .map(error => `${error.property} - ${Object.values(error.constraints).join(', ')}`)
+      
+      throw new ValidationException(messages)
+    }
+    return value
+  }
+
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { IsEmail, IsString, Length } from "class-validator"
 
 export class CreateUserDto {
   
   @ApiProperty({example: 'user@mail.ru', description: 'Mailing address'})
+  @IsString({message: 'Must be a string'})
+  @IsEmail({}, {message: 'Incorrect email'})
   readonly email: string
   
+  
   @ApiProperty({example: '13413434', description: 'Password'})
+  @IsString({message: 'Must be a string'})
+  @Length(4, 16, {message: 'Not less than 4 and not more than 16'})
   readonly password: string
 }
